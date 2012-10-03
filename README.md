184df01fc69d8da80cba5db41a409475c286c836
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-10-03 17:58:20 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redmine Reports & Charts plugin
+
+Redmine plugin for reports and charts
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_reports do
+  name 'Redmine Reports & Charts plugin'
+  author 'Danil Tashkinov'
+  description 'Redmine plugin for reports and charts'
+  version '0.0.1'
+  url 'https://github.com/noddecarter/redmine_reports'
+  author_url 'https://github.com/Undev'
+end
